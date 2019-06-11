c4c24da668d234759d0e9eaef019dd69b7d5588d
gackle <gackle83@outlook.com>
2019-06-11 16:06:58 +0800

    使用输出到标准错误的 eprintln! 宏

--- src/main.rs
+++ src/main.rs
@@ -12,12 +12,12 @@ fn main() {
     // 然而，当其值为 Err 时，该方法会调用一个闭包（closure），也就是一个我们定义的作为参数传递给 unwrap_or_else 的匿名函数。
     // 传递给闭包中位于两道竖线间的参数 err。闭包中的代码在其运行时可以使用这个 err 值。
     let config = Config::new(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {}", err);
+        eprintln!("Problem parsing arguments: {}", err);
         process::exit(1);
     });
 
     if let Err(e) = grep_rs::run(config) {
-        println!("Application error: {}", e);
+        eprintln!("Application error: {}", e);
         process::exit(1);
     }
 }
