dfde30f8cb7a082289918c91668a3ce91e5584e4
gackle <gackle83@outlook.com>
2019-06-11 15:55:08 +0800

    添加大小写不敏感的 search 以及其测试

--- src/lib.rs
+++ src/lib.rs
@@ -26,7 +26,7 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
         println!("{}", line);
     }
 
-    Ok(())  // 返回 unit 类型 ()
+    Ok(()) // 返回 unit 类型 ()
 }
 
 fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
@@ -43,20 +43,45 @@ fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
     results
 }
 
+fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let query = query.to_lowercase();
+    let mut results = Vec::new();
+
+    for line in contents.lines() {
+        if line.to_lowercase().contains(&query) {
+            results.push(line);
+        }
+    }
+
+    results
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
-    fn one_result() {
+    fn case_sensitive() {
         let query = "duct";
         let contents = "\
 Rust:
 safe, fast, productive.
-Pick three.";
+Pick three.
+Duct tape.";
+
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+    }
+
+    #[test]
+    fn case_insensitive() {
+        let query = "rUsT";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.
+Trust me.";
         assert_eq!(
-            vec!["safe, fast, productive."], 
-            search(query, contents)
-        );
+            vec!["Rust:", "Trust me."], 
+            search_case_insensitive(query, contents));
     }
 }
