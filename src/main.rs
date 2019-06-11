811aa3b789b1c51379b2a8c0c3367fcfea61e641
gackle <gackle83@outlook.com>
2019-06-11 11:30:35 +0800

    将参数值保存进变量

--- src/main.rs
+++ src/main.rs
@@ -2,5 +2,14 @@ use std::env;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    println!("{:?}", args);
+    
+    if args.len() < 3 {
+        panic!("Less than 3 paramters.")
+    }
+
+    let query = &args[1];
+    let filename = &args[2];
+
+    println!("Searching for {}", query);
+    print!("In file {}", filename);
 }
